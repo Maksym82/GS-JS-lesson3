51780f0500dfa2c33dc805854473432c3f05be53
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-11-07 14:00:34 +0200

    Update script.js

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -9,20 +9,43 @@
 //   { id: 3, name: "Orange" },
 // ];
 
+// const names = fruts.map((fruit) => fruit.name);
+// console.log(names);
+
 //  -- 2 --
 //Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 !== 0) continue;
+//   console.log(i);
+// }
+
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
 
 //  -- 3 --
 //Замініть цикл "for" на "while"
 // for (let i = 0; i < 5; i++) {
-//     console.lpg( `цифра ${i}!` );
-//   }
+//   console.log(`цифра ${i}!`);
+// }
+// let i = 0;
+// while (i < 5) {
+//   i++;
+//   console.log(`цифра ${i}!`);
+// }
 
 //  -- 4 --
 //Напишіть цикл, який пропонує prompt ввести число більше за 100.
 //Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
 //Цикл повинен запитувати число доти, доки відвідувач не введе число,
 // більше за 100, або не скасує ввід/введе порожній рядок.
+
+// let number;
+// do {
+//   number = +prompt("Введите число больше 100");
+// } while (number !== null && number !== "" && number <= 100);
+// console.log(number);
 
 //  -- 5 --
 // Вирахуйте середній вік
@@ -33,3 +56,8 @@
 //   { age: 10, name: "Юля" },
 //   { age: 20, name: "Катя" },
 // ];
+
+// const totalAge = girls.reduce((sum, girl) => sum + girl.age, 0);
+// const result = totalAge / girls.length;
+
+// console.log(`Средний возраст: ${result}`);
